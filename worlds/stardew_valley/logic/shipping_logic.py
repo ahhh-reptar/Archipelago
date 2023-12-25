355c84be5b3f0d4de1e49f2aa7b886fbc09471eb
Jouramie <jouramie@hotmail.com>
2023-12-25 16:45:58 -0500

    convert shipping

--- worlds/stardew_valley/logic/shipping_logic.py
+++ worlds/stardew_valley/logic/shipping_logic.py
@@ -5,14 +5,16 @@ from Utils import cache_self1
 from .base_logic import BaseLogic, BaseLogicMixin
 from .building_logic import BuildingLogicMixin
 from .has_logic import HasLogicMixin
+from .option_logic import OptionLogicMixin
 from .received_logic import ReceivedLogicMixin
 from .region_logic import RegionLogicMixin
+from .. import options
 from ..locations import LocationTags, locations_by_tag
-from ..options import ExcludeGingerIsland
-from ..options import SpecialOrderLocations
 from ..stardew_rule import StardewRule, And
-from ..strings.building_names import Building
 from ..strings.ap_names.event_names import Event
+from ..strings.building_names import Building
+
+shipsanity_prefix = "Shipsanity: "
 
 
 class ShippingLogicMixin(BaseLogicMixin):
@@ -21,7 +23,7 @@ class ShippingLogicMixin(BaseLogicMixin):
         self.shipping = ShippingLogic(*args, **kwargs)
 
 
-class ShippingLogic(BaseLogic[Union[ReceivedLogicMixin, ShippingLogicMixin, BuildingLogicMixin, RegionLogicMixin, HasLogicMixin]]):
+class ShippingLogic(BaseLogic[Union[ReceivedLogicMixin, ShippingLogicMixin, BuildingLogicMixin, RegionLogicMixin, HasLogicMixin, OptionLogicMixin]]):
 
     @cached_property
     def can_use_shipping_bin(self) -> StardewRule:
@@ -32,17 +34,20 @@ class ShippingLogic(BaseLogic[Union[ReceivedLogicMixin, ShippingLogicMixin, Buil
         return self.logic.received(Event.can_ship_items) & self.logic.has(item)
 
     def can_ship_everything(self) -> StardewRule:
-        shipsanity_prefix = "Shipsanity: "
-        all_items_to_ship = []
-        exclude_island = self.options.exclude_ginger_island == ExcludeGingerIsland.option_true
-        exclude_qi = self.options.special_order_locations != SpecialOrderLocations.option_board_qi
-        mod_list = self.options.mods.value
-        for location in locations_by_tag[LocationTags.SHIPSANITY_FULL_SHIPMENT]:
-            if exclude_island and LocationTags.GINGER_ISLAND in location.tags:
-                continue
-            if exclude_qi and LocationTags.REQUIRES_QI_ORDERS in location.tags:
-                continue
-            if location.mod_name and location.mod_name not in mod_list:
-                continue
-            all_items_to_ship.append(location.name[len(shipsanity_prefix):])
-        return self.logic.building.has_building(Building.shipping_bin) & And(*(self.logic.has(item) for item in all_items_to_ship))
+        def create_rule(exclude_ginger_island, special_order_locations, mods):
+            all_items_to_ship = []
+            exclude_island = exclude_ginger_island == options.ExcludeGingerIsland.option_true
+            exclude_qi = special_order_locations != options.SpecialOrderLocations.option_board_qi
+            mod_list = mods
+            for location in locations_by_tag[LocationTags.SHIPSANITY_FULL_SHIPMENT]:
+                if exclude_island and LocationTags.GINGER_ISLAND in location.tags:
+                    continue
+                if exclude_qi and LocationTags.REQUIRES_QI_ORDERS in location.tags:
+                    continue
+                if location.mod_name and location.mod_name not in mod_list:
+                    continue
+                all_items_to_ship.append(location.name[len(shipsanity_prefix):])
+            return self.logic.building.has_building(Building.shipping_bin) & And(*(self.logic.has(item) for item in all_items_to_ship))
+
+        return self.logic.option.custom_rule(options.ExcludeGingerIsland, options.SpecialOrderLocations, options.Mods,
+                                             rule_factory=create_rule)
