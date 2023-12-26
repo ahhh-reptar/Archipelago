8dea7873778f99e91389a12e24316edf44a51493
Jouramie <jouramie@hotmail.com>
2023-12-25 23:49:59 -0500

    handle region logic

--- worlds/stardew_valley/logic/region_logic.py
+++ worlds/stardew_valley/logic/region_logic.py
@@ -1,24 +1,25 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 from Utils import cache_self1
 from .base_logic import BaseLogic, BaseLogicMixin
-from ..options import EntranceRandomization
-from ..stardew_rule import StardewRule, And, Or, Reach, Count, True_
+from .option_logic import OptionLogicMixin
+from .. import options
+from ..stardew_rule import StardewRule, And, Or, Reach, Count, true_
 from ..strings.region_names import Region
 
-main_outside_area = {Region.menu, Region.stardew_valley, Region.farm_house, Region.farm, Region.town, Region.beach, Region.mountain, Region.forest,
-                     Region.bus_stop, Region.backwoods, Region.bus_tunnel, Region.tunnel_entrance}
-always_accessible_regions_without_er = {*main_outside_area, Region.community_center, Region.pantry, Region.crafts_room, Region.fish_tank, Region.boiler_room,
-                                        Region.vault, Region.bulletin_board, Region.mines, Region.hospital, Region.carpenter, Region.alex_house,
-                                        Region.elliott_house, Region.ranch, Region.farm_cave, Region.wizard_tower, Region.tent, Region.pierre_store,
-                                        Region.saloon, Region.blacksmith, Region.trailer, Region.museum, Region.mayor_house, Region.haley_house,
-                                        Region.sam_house, Region.jojamart, Region.fish_shop}
+always_accessible_main_outside_area = {Region.menu, Region.stardew_valley, Region.farm_house, Region.farm, Region.town, Region.beach, Region.mountain,
+                                       Region.forest, Region.bus_stop, Region.backwoods, Region.bus_tunnel, Region.tunnel_entrance}
+always_accessible_inside_with_simple_er_settings = {Region.community_center, Region.pantry, Region.crafts_room, Region.fish_tank, Region.boiler_room,
+                                                    Region.vault, Region.bulletin_board, Region.mines, Region.hospital, Region.carpenter, Region.alex_house,
+                                                    Region.elliott_house, Region.ranch, Region.farm_cave, Region.wizard_tower, Region.tent, Region.pierre_store,
+                                                    Region.saloon, Region.blacksmith, Region.trailer, Region.museum, Region.mayor_house, Region.haley_house,
+                                                    Region.sam_house, Region.jojamart, Region.fish_shop}
 
-always_regions_by_setting = {EntranceRandomization.option_disabled: always_accessible_regions_without_er,
-                             EntranceRandomization.option_pelican_town: always_accessible_regions_without_er,
-                             EntranceRandomization.option_non_progression: always_accessible_regions_without_er,
-                             EntranceRandomization.option_buildings: main_outside_area,
-                             EntranceRandomization.option_chaos: always_accessible_regions_without_er}
+always_regions_by_setting = {options.EntranceRandomization.option_disabled: always_accessible_inside_with_simple_er_settings,
+                             options.EntranceRandomization.option_pelican_town: always_accessible_inside_with_simple_er_settings,
+                             options.EntranceRandomization.option_non_progression: always_accessible_inside_with_simple_er_settings,
+                             options.EntranceRandomization.option_buildings: always_accessible_main_outside_area,
+                             options.EntranceRandomization.option_chaos: always_accessible_inside_with_simple_er_settings}
 
 
 class RegionLogicMixin(BaseLogicMixin):
@@ -27,12 +28,17 @@ class RegionLogicMixin(BaseLogicMixin):
         self.region = RegionLogic(*args, **kwargs)
 
 
-class RegionLogic(BaseLogic[RegionLogicMixin]):
+class RegionLogic(BaseLogic[Union[RegionLogicMixin, OptionLogicMixin]]):
 
     @cache_self1
     def can_reach(self, region_name: str) -> StardewRule:
-        if region_name in always_regions_by_setting[self.options.entrance_randomization]:
-            return True_()
+        if region_name in always_accessible_main_outside_area:
+            return true_
+
+        if region_name in always_accessible_inside_with_simple_er_settings:
+            return self.logic.option.choice_or_true(options.EntranceRandomization,
+                                                    value=options.EntranceRandomization.option_buildings,
+                                                    match=Reach(region_name, "Region"))
 
         return Reach(region_name, "Region")
 
