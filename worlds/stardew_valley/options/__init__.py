7b7545187199a79094651167ce727d84c6fccff8
Alex Gilbert <alexgilbert@yahoo.com>
2024-12-28 01:09:09 -0500

    - Add Split backpack feature. It probably works

--- worlds/stardew_valley/options/__init__.py
+++ worlds/stardew_valley/options/__init__.py
@@ -2,5 +2,5 @@ from .options import StardewValleyOption, Goal, FarmType, StartingMoney, ProfitM
     SeasonRandomization, Cropsanity, BackpackProgression, ToolProgression, ElevatorProgression, SkillProgression, BuildingProgression, FestivalLocations, \
     ArcadeMachineLocations, SpecialOrderLocations, QuestLocations, Fishsanity, Museumsanity, Monstersanity, Shipsanity, Cooksanity, Chefsanity, Craftsanity, \
     Friendsanity, FriendsanityHeartSize, Booksanity, Walnutsanity, NumberOfMovementBuffs, EnabledFillerBuffs, ExcludeGingerIsland, TrapItems, \
-    MultipleDaySleepEnabled, MultipleDaySleepCost, ExperienceMultiplier, FriendshipMultiplier, DebrisMultiplier, QuickStart, Gifting, Mods, BundlePlando, \
-    StardewValleyOptions
+    MultipleDaySleepEnabled, MultipleDaySleepCost, ExperienceMultiplier, FriendshipMultiplier, DebrisMultiplier, BackpackSize, QuickStart, Gifting, Mods,\
+    BundlePlando, StardewValleyOptions
