4e4c4a69bd89699eae534843aafe2ec3b6f2037c
Alex Gilbert <alexgilbert@yahoo.com>
2025-02-18 23:42:55 -0500

    - Add haunted and enlarged traps

--- worlds/dungeon_clawler/constants/filler_names.py
+++ worlds/dungeon_clawler/constants/filler_names.py
@@ -21,3 +21,5 @@ class Trap:
     meli_bomb_trap = create_trap("Meli-Bomb Trap")
     poisonous_spore_trap = create_trap("Poisonous Spore Trap")
     ink_trap = create_trap("Ink Trap")
+    haunted_trap = create_trap("Haunted Trap")
+    enlarged_trap = create_trap("Enlarged Trap")
