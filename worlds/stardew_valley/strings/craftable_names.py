354cb9187ee6e68e42410c9fce0e59642d7a6f90
ahhh-reptar <87342843+ahhh-reptar@users.noreply.github.com>
2025-06-25 12:19:56 -0600

    adjusted both crafts to point to the same string and removed the spare

--- worlds/stardew_valley/strings/craftable_names.py
+++ worlds/stardew_valley/strings/craftable_names.py
@@ -167,7 +167,6 @@ class Craftable:
 
 class ModEdible:
     seed_cookie = "Seed Cookie"
-    svedl_marsh_tonic = "Marsh Tonic"
     sve_marsh_tonic = "Marsh Tonic"
     sve_bombardier_elixir = "Bombardier Elixir"
     magic_elixir = "Magic Elixir"
