6f7dc168ebb1578d407d0cd375ba34ed70539d74
ahhh-reptar <87342843+ahhh-reptar@users.noreply.github.com>
2025-07-03 16:02:10 -0600

    attempted to add resources support and fix code for other parts of HardModCombatExclusions

--- worlds/stardew_valley/locations.py
+++ worlds/stardew_valley/locations.py
@@ -7,7 +7,6 @@ from typing import Optional, Dict, Protocol, List, Iterable
 from . import data, content
 from .bundles.bundle_room import BundleRoom
 from .content.game_content import StardewContent
-from .content.mods.hardmodcombat import HardModCombatBosses, HardModCombatElites, HardModCombatResources
 from .content.vanilla.ginger_island import ginger_island_content_pack
 from .content.vanilla.qi_board import qi_board_content_pack
 from .data.game_item import ItemTag
@@ -29,6 +28,7 @@ LOCATION_CODE_OFFSET = 717000
 
 
 class LocationTags(enum.Enum):
+    HARD_MOD_COMBAT_RESOURCES = enum.auto()
     HARD_MOD_COMBAT_ELITES = enum.auto()
     HARD_MOD_COMBAT_BOSSES = enum.auto()
     MANDATORY = enum.auto()
@@ -765,12 +765,19 @@ def filter_masteries_locations(content: StardewContent, locations: Iterable[Loca
 def filter_modded_locations(locations: Iterable[LocationData], content: StardewContent) -> Iterable[LocationData]:
     return (location for location in locations if content.are_all_enabled(location.content_packs))
 
-def filter_hard_mod_combat_bosses_locations(content: StardewContent, locations: Iterable[LocationData]) -> Iterable[LocationData]:
-    mod_bosses_disabled = content.is_enabled(HardModCombatBosses)
+def filter_hard_mod_combat_bosses_locations(locations: Iterable[LocationData]) -> Iterable[LocationData]:
+    mod_bosses_disabled = options.HardModCombatExclusions(1)
     return (location for location in locations if mod_bosses_disabled or LocationTags.HARD_MOD_COMBAT_BOSSES not in location.tags)
 
 def filter_hard_mod_combat_elites_locations(locations: Iterable[LocationData]) -> Iterable[LocationData]:
-    return (location for location in locations if LocationTags.HARD_MOD_COMBAT_BOSSES or LocationTags.HARD_MOD_COMBAT_ELITES not in location.tags)
+    mod_elites_disabled = options.HardModCombatExclusions(2)
+    return (location for location in locations if mod_elites_disabled or LocationTags.HARD_MOD_COMBAT_BOSSES or
+            LocationTags.HARD_MOD_COMBAT_ELITES not in location.tags)
+
+def filter_hard_mod_combat_resources_locations(locations: Iterable[LocationData]) -> Iterable[LocationData]:
+    mod_resources_disabled = options.HardModCombatExclusions(3)
+    return (location for location in locations if mod_resources_disabled or LocationTags.HARD_MOD_COMBAT_BOSSES or
+            LocationTags.HARD_MOD_COMBAT_ELITES or LocationTags.HARD_MOD_COMBAT_RESOURCES not in location.tags)
 
 def filter_disabled_locations(options: StardewValleyOptions, content: StardewContent, locations: Iterable[LocationData]) -> Iterable[LocationData]:
     locations_deprecated_filter = filter_deprecated_locations(locations)
@@ -779,5 +786,7 @@ def filter_disabled_locations(options: StardewValleyOptions, content: StardewCon
     locations_qi_filter = filter_qi_order_locations(content, locations_island_filter)
     locations_masteries_filter = filter_masteries_locations(content, locations_qi_filter)
     locations_mod_filter = filter_modded_locations(locations_masteries_filter, content)
-    locations_hard_mod_combat_bosses_filter = filter_hard_mod_combat_bosses_locations(locations)
+    locations_hard_mod_combat_bosses_filter = filter_hard_mod_combat_bosses_locations(locations_mod_filter)
+    locations_hard_mod_combat_elites_filter = filter_hard_mod_combat_elites_locations(locations_hard_mod_combat_bosses_filter)
+    locations_hard_mod_combat_resources_filter = filter_hard_mod_combat_resources_locations(locations_hard_mod_combat_elites_filter)
     return locations_mod_filter
